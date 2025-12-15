1b5815fa121eba259998cef91b6e0c016a9c0706
qinuan01 <142510302+qinuan01@users.noreply.github.com>
2025-12-15 21:26:30 +0800

    Update Myau.java

--- src/main/java/myau/Myau.java
+++ src/main/java/myau/Myau.java
@@ -66,6 +66,7 @@ public class Myau {
         moduleManager.modules.put(AntiObfuscate.class, new AntiObfuscate());
         moduleManager.modules.put(AntiVoid.class, new AntiVoid());
         moduleManager.modules.put(AutoClicker.class, new AutoClicker());
+        moduleManager.modules.put(AutoMiner.class, new AutoMiner());
         moduleManager.modules.put(AutoAnduril.class, new AutoAnduril());
         moduleManager.modules.put(AutoHeal.class, new AutoHeal());
         moduleManager.modules.put(AutoTool.class, new AutoTool());
